bc29e22091700f3f63554e4d266c26a00eadefa5
Henrik <thkodehode@gmail.com>
2025-03-25 14:43:56 +0100

    gitnore fix

--- obj/Debug/net8.0/backend uke 2 dag 2.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend uke 2 dag 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend uke 2 dag 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b194b321ea036f664dddbe74c127072a2b378c93")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend uke 2 dag 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend uke 2 dag 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
